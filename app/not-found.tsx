94a8a69c816aef0bf8edc46ea313cc02e99c24b3
Vadym Snizhko <snizhko@gmail.com>
2026-07-25 20:09:49 +0300

    add page note0found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,24 @@
 import css from './Home.module.css'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 - Page not found | NoteHub",
+  description: "Sorry, the page you are looking for does not exist.",
+  openGraph: {
+      title: `404 - Page not found | NoteHub`,
+      description: "Sorry, the page you are looking for does not exist.",
+      url: `https://08-zustand-puce-kappa.vercel.app/`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'notehub.jpg',
+          width: 1200,
+          height: 630,
+          alt: "Note Hub",
+        },
+      ]
+    }
+};
 
 const NotFoundPage = () => {
     return (
